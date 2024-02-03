bb08d4b479f2dc7c87209ef582b2c92ddfc564d8
Andrea <admonandreasanchez@gmail.com>
2024-02-02 22:30:25 -0500

    Second try of updating Snake Game Final

---
+++ scoreboard.py
@@ -0,0 +1,58 @@
+
+from turtle import Turtle
+
+INITIAL_SCORE = 0
+ALIGNMENT = 'center'
+FONT = ('Arial', 16, 'normal')
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+        self.penup()
+        self.color('white')
+        self.speed("fastest")
+        self.goto(-10, 270)
+        self.update_scoreboard()
+        self.hideturtle()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", True, align=ALIGNMENT,font=FONT)
+
+    def reset(self):
+        self.goto(-10, 270)
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.penup()
+        self.color('white')
+        self.speed("fastest")
+        self.goto(-10, 270)
+        self.update_scoreboard()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
